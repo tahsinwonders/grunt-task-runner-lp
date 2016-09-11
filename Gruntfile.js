386774403a4aa4e0e28b50a697f5f3919c567424
Tahsin <tahsinwonders@gmail.com>
2016-09-11 04:21:05 -0400

    Grunt Sass Live reload landing page task runner

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,24 +2,14 @@ module.exports = function(grunt) {
 	grunt.initConfig ({
 		// basic setting 
 		pkg: grunt.file.readJSON('package.json'),
-		emailBuilder: {
-		  files: {
-                expand  : true,
-                flatten : true,
-                ext     : '.html', 
-                // root directory
-                src     : ['./non-inline/*.html'],
-                // dstination
-                dest    : './generated-inline/'
-            }
-        },
+		
         sass: {                              // Task
 		    dist: {                            // Target
 		      options: {                       // Target options
 		        style: 'expanded'
 		      },
 		      files: {                         // Dictionary of files
-		        './non-inline/css/main.css': './non-inline/sass/main.scss',       // 'destination': 'source'
+		        'dist/css/main.css': 'dist/scss/main.scss',       // 'destination': 'source'
 		        
 		      }
 		    }
@@ -28,8 +18,8 @@ module.exports = function(grunt) {
 		watch: {
 		    'normal': {
 	    	  options:{livereload:true},
-		      files: ['./non-inline/*.html', './non-inline/css/*.css', './non-inline/sass/**'],
-		      tasks: ['emailBuilder', 'sass'],
+		      files: ['dist/**', 'dist/js/**', 'dist/css/**', 'dist/scss/**'],
+		      tasks: [ 'sass'],
 		    },
 		},
 		express:{
@@ -37,7 +27,7 @@ module.exports = function(grunt) {
                 options:{
                     port:9000,
                     hostname:'localhost',
-                    bases:['./non-inline/'], //where yout localhost:9000/ is
+                    bases:['dist/'], //where yout localhost:9000/ is
                     livereload:true 
                 }
             }
@@ -47,7 +37,6 @@ module.exports = function(grunt) {
 	// load the plug in
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-express');  
-	grunt.loadNpmTasks('grunt-email-builder');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	// do the task
 	grunt.registerTask('server', ['express','watch', 'sass']);
